455c90a14c30146be3c774667b6fa0754b714f41
dmitriy-lar <larionov.dm2002@gmail.com>
2022-11-22 18:20:08 +0300

    Refactorting

--- books/models.py
+++ books/models.py
@@ -4,19 +4,18 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=300)
     genre = models.ManyToManyField("Genres")
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
     created = models.CharField(max_length=20)
     pages = models.IntegerField()
     synopsis = models.TextField()
     additional_information = models.TextField()
 
-
     def __str__(self):
         return self.title
 
+
 class Genres(models.Model):
     title = models.CharField(max_length=300)
-
 
     def __str__(self):
         return self.title
@@ -27,7 +26,6 @@ class Author(models.Model):
     date_of_birth = models.DateField()
     country_of_birth = models.CharField(max_length=100)
     additional_information = models.TextField()
-
 
     def __str__(self):
         return self.name
--- books/serializers.py
+++ books/serializers.py
@@ -2,32 +2,31 @@ from rest_framework import serializers
 
 from .models import Book
 
+
 class BookSerializer(serializers.ModelSerializer):
     detail_url = serializers.HyperlinkedIdentityField(
-        view_name='books-detail',
-        lookup_field='pk'
+        view_name="books-detail", lookup_field="pk"
     )
     delete_url = serializers.HyperlinkedIdentityField(
-        view_name='books-delete',
-        lookup_field='pk'
+        view_name="books-delete", lookup_field="pk"
     )
     edit_url = serializers.HyperlinkedIdentityField(
-        view_name='books-edit',
-        lookup_field='pk'
+        view_name="books-edit", lookup_field="pk"
     )
+
     class Meta:
         model = Book
         fields = [
-            'title',
-            'genre',
-            'author',
-            'created',
-            'pages',
-            'synopsis',
-            'additional_information',
-            'detail_url',
-            'delete_url',
-            'edit_url'
+            "title",
+            "genre",
+            "author",
+            "created",
+            "pages",
+            "synopsis",
+            "additional_information",
+            "detail_url",
+            "delete_url",
+            "edit_url",
         ]
 
 
@@ -35,11 +34,11 @@ class BookCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = [
-            'title',
-            'genre',
-            'author',
-            'created',
-            'pages',
-            'synopsis',
-            'additional_information'
+            "title",
+            "genre",
+            "author",
+            "created",
+            "pages",
+            "synopsis",
+            "additional_information",
         ]
